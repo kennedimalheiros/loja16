0dd2ab942a9ba4673dde9a406a13a3e2187c1171
kennedimalheiros <kennedimalheiros@gmail.com>
2014-05-07 11:13:36 -0300

    ModeloHTML

--- email4/form.php
+++ email4/form.php
@@ -34,9 +34,9 @@ $mail->Subject = 'Titulo do Email';  //Titulo do email
 //Abrindo o arquivo HTML e lendo o seu conteudo
 //$mail->msgHTML(file_get_contents('account.html'), dirname(__FILE__));
 //$mail->msgHTML(file_get_contents('../mails/br/bankwire.html'));
-$msgHTML = (file_get_contents('../mails/br/bankwire.html'));
-//echo $msgHTML;
-$mail->$msgHTML;
+$ModeloHTML = (file_get_contents('../mails/br/bankwire.html'));
+echo $ModeloHTML;
+$mail->$msgHTML($ModeloHTML);
 //$mail->Body    = 'Loja ATS <b>Esta Funcionando \º/!</b>'; //Conteudo do Email com HTML
 //$mail->AltBody = 'Loja ATS Esta Funcionando sem HTML!';   //Conteudo do Email sem HTML
 
