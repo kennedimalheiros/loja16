7f12b3c231a72872a52e16de83767930d7caf9bd
kennedimalheiros <kennedimalheiros@gmail.com>
2014-05-07 10:20:20 -0300

    Referencia email

--- email4/form.php
+++ email4/form.php
@@ -17,7 +17,7 @@ $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl
 
 $mail->From = 'programacaomoc@gmail.com'; //Email de quem esta enviando
 $mail->FromName = 'Loja ATS';  //Nome de quem esta enviando
-$mail->addAddress($EmailDestino, 'Kennedi Malheiros');  // Email do destinatario, Name é opcional
+$mail->addAddress($ClienteEmail, $ClienteNome);  // Email do destinatario, Name é opcional
 //$mail->addAddress('kennedimalheiros@gmail.com', 'Kennedi Malheiros');  // Email do destinatario, Name é opcional
 //$mail->addAddress('kennedimalheiros@gmail.com');               // Outro destinatario, Name é opcional
 $mail->addReplyTo('programacaomoc@gmail.com', 'Responder');    //Email de resposta
