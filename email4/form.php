6f19fe6b8c901a469b190c5f571c3b6a758856b4
kennedimalheiros <kennedimalheiros@gmail.com>
2014-05-07 14:31:56 -0300

    sadasd

--- email4/form.php
+++ email4/form.php
@@ -33,7 +33,7 @@ $mail->Subject = 'Titulo do Email';  //Titulo do email
 
 //Abrindo o arquivo HTML e lendo o seu conteudo
 //$mail->msgHTML(file_get_contents('account.html'), dirname(__FILE__));
-$mail->msgHTML(str_replace('{firstname}', 'KennediMalheiros', file_get_contents('../mails/br/bankwire.html')));
+$mail->msgHTML(str_replace('{firstname}', $ClienteNome, file_get_contents('../mails/br/bankwire.html')));
 
 //$ModeloHTML = (file_get_contents('../mails/br/bankwire.html'));
 
