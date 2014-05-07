3565f5b08add192c9c15da63182cfdb5612b509a
kennedimalheiros <kennedimalheiros@gmail.com>
2014-05-07 08:45:33 -0300

    Alterando email

--- email2/enviarEmail.php
+++ email2/enviarEmail.php
@@ -9,11 +9,11 @@ $assunto = $_POST['assunto'];
 	$mensagem .= "<br> <strong>Mensagem: </strong>".$_POST['mensagem']; 
 	//5 – agora inserimos as codificações corretas e tudo mais. 
 	$headers = "Content-Type:text/html; charset=UTF-8\n"; 
-	$headers .= "From: dominio.com.br<sistema@dominio.com.br>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome 
-	$headers .= "X-Sender: <sistema@dominio.com.br>\n";
+	$headers .= "From: dominio.com.br<programacaomoc@gmail.com>\n"; //Vai ser //mostrado que o email partiu deste email e seguido do nome 
+	$headers .= "X-Sender: <programacaomoc@gmail.com>\n";
 	//email do servidor //que enviou $headers .= "X-Mailer: PHP v".phpversion()."\n"; 
 	$headers .= "X-IP: ".$_SERVER['REMOTE_ADDR']."\n"; 
-	$headers .= "Return-Path: <sistema@dominio.com.br>\n"; //caso a msg //seja respondida vai para este email. 
+	$headers .= "Return-Path: <programacaomoc@gmail.com>\n"; //caso a msg //seja respondida vai para este email. 
 	$headers .= "MIME-Version: 1.0\n"; 
 	
 	mail($para, $assunto, $mensagem, $headers); //função que faz o envio do email. 
