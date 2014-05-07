36e83281c4de787afc572320897a276a420295af
kennedimalheiros <kennedimalheiros@gmail.com>
2014-05-07 10:06:24 -0300

    parametro

--- email4/form.php
+++ email4/form.php
@@ -10,22 +10,23 @@ $mail->Username = 'programacaomoc@gmail.com';            // SMTP username
 $mail->Password = 'programacao';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
 
-$mail->From = 'programacaomoc@gmail.com';
-$mail->FromName = 'Mailer';
-$mail->addAddress('avuldarte@gmail.com', 'Alex Programador');  // Email do destinatario
-$mail->addAddress('kennedimalheiros@gmail.com');               // Name is optional
-$mail->addReplyTo('programacaomoc@gmail.com', 'Responder'); //Email de resposta
-$mail->addCC('kennedi.malheiros@atsinformatica.com.br');
-$mail->addBCC('kennedi.malheiros@atsinformatica.com.br');
+$mail->From = 'programacaomoc@gmail.com'; //Email de quem esta enviando
+$mail->FromName = 'Loja ATS';  //Nome de quem esta enviando
+$mail->addAddress($_GET['emailDest' ], 'Kennedi Malheiros');  // Email do destinatario, Name é opcional
+//$mail->addAddress('kennedimalheiros@gmail.com', 'Kennedi Malheiros');  // Email do destinatario, Name é opcional
+//$mail->addAddress('kennedimalheiros@gmail.com');               // Outro destinatario, Name é opcional
+$mail->addReplyTo('programacaomoc@gmail.com', 'Responder');    //Email de resposta
+//$mail->addCC('kennedi.malheiros@atsinformatica.com.br');       
+//$mail->addBCC('kennedi.malheiros@atsinformatica.com.br');
 
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
 $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'Loja ATS <b>Esta Funcionando \º/!</b>';
-$mail->AltBody = 'Loja ATS Esta Funcionando sem HTML!';
+$mail->Subject = 'Titulo do Email';  //Titulo do email
+$mail->Body    = 'Loja ATS <b>Esta Funcionando \º/!</b>'; //Conteudo do Email com HTML
+$mail->AltBody = 'Loja ATS Esta Funcionando sem HTML!';   //Conteudo do Email sem HTML
 
 if(!$mail->send()) {
    echo 'Mensagem não pôde ser enviada.';
